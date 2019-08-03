ce607b6b07585500fc238bc7e5c3f1882bfe25fb
Amelia Milne <amelia.milne@gmail.com>
2019-08-03 15:17:48 -0700

    cleaner with more comments

--- hypergraph.py
+++ hypergraph.py
@@ -1,10 +1,7 @@
 import numpy as np
 from numpy import linalg as LA
 import itertools
-import math
 
-#  A hypergraph consists of its faces (h_graph) the number of qubits
-#  it has (num_v) and the generators of it's stabilizer
 
 # utility
 x_matrix = np.array([[0, 1],
@@ -29,7 +26,6 @@ plus_state = np.array([[np.sqrt(.5)],
 def construct_hadamard(num_v, q):
     if q == 0:
         out = h_matrix
-
     else:
         out = i_matrix
     for i in range(1, num_v):
@@ -47,12 +43,14 @@ def initialize_i(num_v):
         i = np.kron(i, i_matrix)
     return i
 
+
 # Initialize state as |+>_n : O(n)
-def inititalize_state(num_v):
+def initialize_state(num_v):
     state = plus_state
     for n in range(num_v-1):
         state = np.kron(state, plus_state)
     return state
+
 
 # Create matrices Z_1 to Z_n : O(n^2)
 def initialize_loz(num_v):
@@ -70,10 +68,10 @@ def initialize_loz(num_v):
     return loz
 
 
+# Create measurement operators
 def create_m_ops(num_v):
     set_p = {}
-    primes = (3, 5, 7, 11, 13)
-    for case in list(itertools.product([1, 2], repeat=int(num_v - 2))):
+    for case in list(itertools.product([0, 1], repeat=int(num_v - 2))):
         # create unique key for case
         key = gen_case_key(case)
         # create proj matrix for measurement case
@@ -84,11 +82,14 @@ def create_m_ops(num_v):
             else:
                 p = np.kron(p, (i_matrix + x_matrix) / 2)
         p = np.kron(p, i_matrix)
-        # update case key
+        # update proj matrix set for case
         set_p[key] = p
     return set_p
 
 
+# Create extraction matrices
+# extr_ms[(n-1)th] is the nth extraction matrix assuming measurement result 0
+# extr_ms[2*(n-1)th] is the nth extraction matrix assuming measurement result 1
 def create_extr_matrices(num_v):
     extr_ms = [0] * (num_v - 2) * 2
     for a in range(num_v - 2):
@@ -107,19 +108,20 @@ def create_extr_matrices(num_v):
     return extr_ms
 
 
+# Create Hadamard matrices used with extraction matrices - on qubit 1 with decreasing # of qubits
 def create_loh(num_v):
     loh = [0] * (num_v - 2)
     for i in range(num_v-2):
         loh[i] = construct_hadamard(num_v-i, 1)
-    had_1_on_3 = np.kron(np.kron(i_matrix, h_matrix), i_matrix)
     return loh
 
 
+# Generate unique key based on case
 def gen_case_key(case):
     primes = (5, 7, 11, 13, 17)
     key = 0
     for i in range(len(case)):
-        key = key + (case[i] * primes[i])
+        key = key + ((case[i]+1) * primes[i])
     return key
 
 
@@ -129,15 +131,13 @@ class Hypergraph:
         self.loz = initialize_loz(num_v)
         self.i = initialize_i(num_v)
         self.m_ops = create_m_ops(num_v)
-        # extr_ms[(n-1)th] is the nth extraction with measurement 0
-        # extr_ms[2*(n-1)th] is the nth extraction with measurement 1
         self.extr_ms = create_extr_matrices(num_v)
         self.loh = create_loh(num_v)
         self.h_state = 0
 
     # Construct state from CZ matrices : O(n) for small cases
     def construct_state(self, faces):
-        self.h_state = inititalize_state(self.num_v)
+        self.h_state = initialize_state(self.num_v)
         for f in faces:
             cz = self.i - 2 * ((self.i - self.loz[f[0]]) / 2).dot((self.i - self.loz[f[1]]) / 2).dot(((self.i - self.loz[f[2]]) / 2))
             self.h_state = cz.dot(self.h_state)
@@ -158,7 +158,7 @@ class Hypergraph:
     def extract_bits(self, case):
         for i in range(self.num_v - 2):
             had = self.loh[i]
-            if case[i] == 2:
+            if case[i] == 0:
                 extr_m_0 = self.extr_ms[i]
                 self.h_state = extr_m_0.dot(had.dot(self.h_state))
             else:
--- runhypergraph.py
+++ runhypergraph.py
@@ -1,70 +1,65 @@
 from hypergraph import *
-import math
 import itertools
+import time
 
 # Find hypergraph states on a set Ω = I + M + O of qubits where there exists Bell type entanglement between
 # regions I and O after the qubits in M have been measured in the local X-basis
 
+start_time = time.time()
+
+# num_v is number of qubits (vertices in hypergrah)
 num_v = 7
 possible_faces = []
-m = (1, 2, 3,4,5)
-
-
-def check_each_v_in_a_face(face_list):
-    v_set = {0:0, }
-    count = 0
-    for i in range(1, num_v):
-        v_set[i] = 0
-    for f in face_list:
-        for v in f:
-            if v_set[v] == 0:
-                count = count + 1
-                v_set[v] = 1
-    if count == num_v:
-        return True
-    return False
-
+# m is the measurement set
+m = (1, 2, 3, 4, 5)
 
 # create all possible faces given num_v
 for face in itertools.combinations(range(0, num_v), 3):
     possible_faces.append(face)
 
+# create utility for hypergraph with num_v number of qubits
+# create all Z, I, Projection(for measurement), Extraction and Hadamard matrices
+
+#before_h_class_time = time.time()
 
 h = Hypergraph(num_v)
 
+#after_h_class_time = time.time()
+#print("Time to create Hypergraph:", after_h_class_time - before_h_class_time)
+
 # create all possible hypergraphs taking each combination of faces
 for i in range(len(possible_faces)):
-    i=4
+    print(len(possible_faces))
     for faces in itertools.combinations(possible_faces, i+1):
-        each_v_in_a_face = True
-        each_v_in_a_face = check_each_v_in_a_face(faces)
-        if each_v_in_a_face:
-            case_list = []
-            prob_list = []
-            h.construct_state(faces)
-            # 1 represents measurement result 1
-            # 2 represents measurement result 0
-            for case in list(itertools.product([1, 2], repeat=len(m))):
-                save_h_state = h.h_state
-                m_took = h.perform_measurement(case)
-                # probability of measurement case occuring
-                prob = np.transpose(save_h_state).dot(m_took.dot(save_h_state))
-                h.extract_bits(case)
-                rho = h.h_state.dot(np.transpose(h.h_state))
-                partial_rho = np.trace(rho.reshape([2, 2, 2, 2]), axis1=1, axis2=3)
-                w, v = LA.eig(partial_rho)
-                if np.allclose(w, np.array([0.5, 0.5])):
-                    case_list.append(case)
-                    prob_list.append(prob)
-                h.h_state = save_h_state
-            if np.isclose(1, sum(prob_list)):
-                print("Faces: ", faces, " with cases", case_list, "with total prob", sum(prob_list))
-    break
+        case_list = []
+        prob_list = []
+        h.construct_state(faces)
+        # case[k] == 0 represents measurement result of m[k] == 0
+        # case[k] == 1 represents measurement result of m[k] == 1
+        for case in list(itertools.product([0, 1], repeat=len(m))):
+            save_h_state = h.h_state
+            # m_took is the measurement operator used on the state (saved to find prob)
+            m_took = h.perform_measurement(case)
+            # probability of measurement case occurring
+            prob = np.transpose(save_h_state).dot(m_took.dot(save_h_state))
+            # extract the bits that were measured
+            h.extract_bits(case)
 
+            # check if resulting qubits are maximally entangled using density matrix
+            rho = h.h_state.dot(np.transpose(h.h_state))
+            partial_rho = np.trace(rho.reshape([2, 2, 2, 2]), axis1=1, axis2=3)
+            w, v = LA.eig(partial_rho)
+            if np.allclose(w, np.array([0.5, 0.5])):
+                case_list.append(case)
+                prob_list.append(prob)
+            # revert h_state back to its pre-measurement state to try the next case
+            h.h_state = save_h_state
+        if np.isclose(1, sum(prob_list)):
+            print("Faces: ", faces, " with cases", case_list, "with total prob", sum(prob_list))
 
+end_time = time.time()
 
-
-
+print("Total time:", end_time - start_time)
 
 
 
